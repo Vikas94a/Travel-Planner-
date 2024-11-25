f08a83556e277d023c7a2e93a7b91271fab54268
Vikas94a <vikas1.kodehode@gmail.com>
2024-11-25 10:06:17 +0100

    commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const suggestionApiKey = "AIzaSyCSmXlBrbDs6apjS5GrDYaTdcxLT1BbbB0";
+const suggestionApiKey = "XYZ";
 const suggestionApiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants&location=40.714728,-73.998672&radius=500&key=${suggestionApiKey}`;
 
 console.log(suggestionApiUrl);
@@ -7,3 +7,12 @@ function autoSuggestion(inputTag) {
   const autoComplete = new google.maps.places.Autocomplete(inputTag);
 }
 autoSuggestion(destination);
+
+async function currencyConverter(Nok) {
+  const converterURL = `https://latest.currency-api.pages.dev/v1/currencies/nok.json`;
+}
+
+// flag api https://flagsapi.com/BE/flat/64.png
+// currency api https://latest.currency-api.pages.dev/v1/currencies/nok.json
+
+// https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json
